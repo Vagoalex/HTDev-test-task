9421ffc226703645478df8999ca1d33cc3a43f89
Vago Aleksandr <vgoalex666@mail.ru>
2022-08-06 22:33:11 +0300

    feat: complete desktop variant with logic. to do: adaptive)

--- src/components/pages/one-note-page/note-page-layout/NotePageLayout.tsx
+++ src/components/pages/one-note-page/note-page-layout/NotePageLayout.tsx
@@ -5,13 +5,18 @@ import { INote } from '../../../../types/notesTypes';
 import './NotePageLayout.scss';
 
 const NotePageLayout: FC<INote> = ({ noteNumber, text, signature, time }) => {
+  const validText =
+    text === ''
+      ? 'Тут должен был быть текст, но его не ввели при создании заметки, так что вот вам месседж от меня, Hello World!!!111'
+      : text;
+
   return (
     <div className='NotePageLayout'>
       <div>
         <h4 className='NotePageLayout__signature'>{signature}</h4>
         <h2 className='NotePageLayout__title'>{`Заметка №${noteNumber}`}</h2>
         <h2 className='NotePageLayout__time'>{time}</h2>
-        <p className='NotePageLayout__desk'>{text}</p>
+        <p className='NotePageLayout__desk'>{validText}</p>
       </div>
       <Link to='/notes' className='NotePageLayout__back'>
         Вернуться обратно
